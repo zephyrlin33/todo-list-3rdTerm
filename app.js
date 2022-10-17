b8c6b737489eb1253fc6eddde586ab9cb3822a81
zephyrlin33 <zephyrlin33@gmail.com>
2022-10-18 03:53:21 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars');
 const Todo = require('./models/todo') // 載入 Todo model
+const bodyParser = require('body-parser')// 引用 body-parser
 
 const app = express()
 //資料庫連線設定
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
 
 const db = mongoose.connection
 // 連線異常
@@ -21,7 +19,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-
+//規定express引擎並使用hbs作為預設副檔名
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 // 設定首頁路由
 app.get('/', (req, res) => {
     Todo.find() // 取出 Todo model 裡的所有資料
@@ -30,6 +32,17 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error)) // 錯誤處理
 })
 
+app.get('/todos/new', (req, res) => {
+    return res.render('new')
+  })
+
+
+app.post('/todos', (req, res) => {
+    const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+    return Todo.create({ name })     // 存入資料庫
+      .then(() => res.redirect('/')) // 新增完成後導回首頁
+      .catch(error => console.log(error))
+  })
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
