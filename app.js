9b2085de1116f2db0c610293a2bff776bcfcf821
zephyrlin33 <zephyrlin33@gmail.com>
2022-10-18 04:16:45 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ app.get('/todos/new', (req, res) => {
     return res.render('new')
   })
 
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .lean()
+      .then((todo) => res.render('detail', { todo }))
+      .catch(error => console.log(error))
+  })
 
 app.post('/todos', (req, res) => {
     const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
