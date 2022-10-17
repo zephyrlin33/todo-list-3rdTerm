cf68776db58635b7f06f69ee647b428bdbfbe821
zephyrlin33 <zephyrlin33@gmail.com>
2022-10-18 04:26:05 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -44,12 +44,33 @@ app.get('/todos/:id', (req, res) => {
       .catch(error => console.log(error))
   })
 
+app.get('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .lean()
+      .then((todo) => res.render('edit', { todo }))
+      .catch(error => console.log(error))
+  })
+
 app.post('/todos', (req, res) => {
     const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
     return Todo.create({ name })     // 存入資料庫
       .then(() => res.redirect('/')) // 新增完成後導回首頁
       .catch(error => console.log(error))
   })
+
+app.post('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    const name = req.body.name
+    return Todo.findById(id)
+      .then(todo => {
+        todo.name = name
+        return todo.save()
+      })
+      .then(()=> res.redirect(`/todos/${id}`))
+      .catch(error => console.log(error))
+  })
+
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
