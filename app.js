805b5591182914ee10d8d903d5a83e09382fb3d9
zephyrlin33 <zephyrlin33@gmail.com>
2022-10-21 23:10:32 +0800

    add sort function

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 設定首頁路由
 app.get('/', (req, res) => {
     Todo.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .lean()
+    .sort({ _id: 'asc' }) // desc降冪
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
