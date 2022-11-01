bceee50846eb9947e6a10b6a7fba978fe518f76e
zephyr Lin <zephyrlin33@gmail.com>
2022-11-01 22:55:52 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = express.Router()
 
 const home = require('./modules/home')// 引入 home 模組程式碼
 const todos = require('./modules/todos')// 引入 todos 模組程式碼
-
+const users = require('./modules/users')  // add this
 
 router.use('/', home)// 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/todos', todos)// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
+router.use('/users', users)  // add this
 
 module.exports = router
