729985545a0ede7eafe64ed86be1056f17778179
zephyr Lin <zephyrlin33@gmail.com>
2022-11-01 23:11:22 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,4 +3,12 @@ const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
+// routes/modules/users.js
+router.post('/login', (req, res) => {
+})
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 module.exports = router
