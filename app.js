c14d75240d01f7d63f0285c651d6fb24184bebbb
zephyr Lin <zephyrlin33@gmail.com>
2022-11-03 02:38:34 +0800

    user login feature via passport

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')//
+
+const usePassport = require('./config/passport')// 載入設定檔，要寫在 express-session 以後
 const bodyParser = require('body-parser')// 引用 body-parser
 const methodOverride = require('method-override') 
 const routes = require('./routes')//引用路由器
@@ -17,6 +19,7 @@ app.set('view engine', 'hbs')
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 app.use(routes)
 //註冊套件session
 app.use(session({
@@ -24,6 +27,9 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+app.use(passport.initialize())
+app.use(passport.session())
+
 // 設定 port 3000
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
+const passport = require('passport')// 引用 passport
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -40,4 +41,10 @@ router.post('/register', (req, res) => {
   })
   .catch(err => console.log(err))
 })
+
+// 加入 middleware，驗證 request 登入狀態
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 module.exports = router
